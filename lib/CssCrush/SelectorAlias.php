bf41662df63e330c30fd6d359396d5ec51e0b70f
Pete Boere <pete@the-echoplex.net>
2014-02-11 22:20:18 +0000

    Added selector `splat` aliases, which expand based on arguments.
    Similar to what happens in the forms and aria plugins, but easier to author.
    Selector alias definitions are now automatically wrapped in `:any()` if	they are comma separated.

--- lib/CssCrush/SelectorAlias.php
+++ lib/CssCrush/SelectorAlias.php
@@ -14,14 +14,11 @@ class SelectorAlias
     public function __construct($type, $handler)
     {
         $this->type = $type;
+        $this->handler = $handler;
 
         switch ($this->type) {
             case 'alias':
-                $this->handler = new Template($handler);
-                break;
-            case 'callback':
-            case 'splat':
-                $this->handler = $handler;
+                $this->handler = new Template(SelectorAlias::wrap($handler));
                 break;
         }
     }
@@ -29,14 +26,38 @@ class SelectorAlias
     public function __invoke($args)
     {
         $handler = $this->handler;
+        $tokens = Crush::$process->tokens;
+
+        $splat_arg_patt = Regex::make('~#\((?<fallback>{{ ident }})?\)~');
 
         switch ($this->type) {
+            case 'alias':
+                return $handler($args);
             case 'callback':
                 $template = new Template($handler($args));
                 return $template($args);
-            case 'alias':
-            default:
-                return $handler($args);
+            case 'splat':
+                $handler = $tokens->restore($handler, 's');
+                if ($args) {
+                    $list = array();
+                    foreach ($args as $arg) {
+                        $list[] = SelectorAlias::wrap(
+                            $tokens->capture(preg_replace($splat_arg_patt, $arg, $handler), 's')
+                        );
+                    }
+                    $handler = implode(',', $list);
+                }
+                else {
+                    $handler = $tokens->capture(preg_replace_callback($splat_arg_patt, function ($m) {
+                        return $m['fallback'];
+                    }, $handler), 's');
+                }
+                return SelectorAlias::wrap($handler);
         }
+    }
+
+    public static function wrap($str)
+    {
+        return strpos($str, ',') !== false ? ":any($str)" : $str;
     }
 }
