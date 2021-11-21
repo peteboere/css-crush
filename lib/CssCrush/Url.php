caf81dcf1aa9148642ba4bdbcf94e86db3a11d0e
Marcus Förster <git@xerc.de>
2021-11-21 19:31:14 +0100

    [OPTIMIZE] data-uri( .svg )

--- lib/CssCrush/Url.php
+++ lib/CssCrush/Url.php
@@ -170,8 +170,17 @@ class Url
         }
 
         $mime_type = $allowed_file_extensions[$file_ext];
-        $base64 = base64_encode(file_get_contents($file));
-        $this->value = "data:$mime_type;base64,$base64";
+        
+        if('svg' == $file_ext) {
+
+          $string = str_replace(['"','#','<'], ['\'','%23','%3C'], file_get_contents($file));
+          $this->value = "data:$mime_type;utf8,$string";
+        }
+        else {
+
+          $base64 = base64_encode(file_get_contents($file));
+          $this->value = "data:$mime_type;base64,$base64";
+        }
 
         $this->setType('data')->protocol = 'data';
 
